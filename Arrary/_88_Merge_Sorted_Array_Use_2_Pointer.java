c3aa531da464d3f642d26bab00bfb1ac4198289c
lonesen <ngotrilong01001@gmail.com>
2024-09-22 13:13:56 +0700

    update login code

--- Arrary/_88_Merge_Sorted_Array_Use_2_Pointer.java
+++ Arrary/_88_Merge_Sorted_Array_Use_2_Pointer.java
@@ -1,5 +1,6 @@
     public class _88_Merge_Sorted_Array_Use_2_Pointer {
-
+        //xử dụng kĩ thuật 2 con trỏ để chạy và xử lý 
+        //thay vỉ chạy 2 vòng for tốn hiệu năng
         public static  void merge(int[] n1, int m, int[] n2, int n) {
             int i = m - 1;
             int j = n - 1;
@@ -24,7 +25,7 @@
         }
         public static void main(String[] args) {
             int [] n1 = {7,8,9,0,0,0};
-            int [] n2 = {2,5,6};
+            int [] n2 = {3,5,6};
             merge(n1, 3, n2, 3);
             System.out.println("Done");
         }
