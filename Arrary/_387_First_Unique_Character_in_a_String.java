f5144611ec30dcac68750ce40a9c1823299dd7cf
longntwk <longntwk@gmail.com>
2024-10-30 21:35:27 +0700

    Update solution handle character

---
+++ Arrary/_387_First_Unique_Character_in_a_String.java
@@ -0,0 +1,25 @@
+public class _387_First_Unique_Character_in_a_String {
+    public static int firstUniqChar(String s) {
+        char[] arr = s.toCharArray();
+        int[] count = new int[123];
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+            int index = (int)c;
+            count[index]++;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+            int index = (int)c;
+            if(count[index] == 1){
+                return i;
+            }
+        }
+        return  -1;
+    }
+    public static void main(String[] args) {
+        // String s = "leetcode";
+        // String s = "loveleetcode";
+        String s = "aabb";
+        System.out.println(firstUniqChar(s));
+    }
+}
